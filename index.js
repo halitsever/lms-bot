e587eda1bae3ba8d324201f65e974889724e7761
Murat <34791459+murathasev@users.noreply.github.com>
2021-01-17 18:29:44 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+/*  
+==============================Util==============================
+*/
+const ayarlar = require("./veri/ayarlar.json");
+const { S_IFREG } = require("constants");
+const zamanlama = require("util").promisify(setTimeout);
+const { getHeapSpaceStatistics } = require("v8");
+/*  
+==============================Modül==============================
+*/
+const { Hesap, Ders, Aplikasyon } = require("./lib/");
+/*  
+==============================Modül==============================
+*/
+
+Aplikasyon.on("ready", (() => {
+    try {
+      Ders.kontrol();
+    } catch (err) {
+      if (err) console.log(err);
+    }
+  })()
+);
